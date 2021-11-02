2e708955352902038601c45c9d3bf209298c8dae
hanxuefeng <hanxuefeng@netjoy.com>
2021-11-02 16:06:45 +0800

    account的测试用例

--- core/accounts/dao_account_test.go
+++ core/accounts/dao_account_test.go
@@ -41,3 +41,97 @@ func TestAccountDao_GetOne(t *testing.T) {
 		logrus.Error(err)
 	}
 }
+
+func TestAccountDao_GetByUserId(t *testing.T) {
+	err := base.Tx(func(runner *dbx.TxRunner) error {
+		dao := AccountDao{
+			runner: runner,
+		}
+		Convey("通过用户id和账户类型查询账户数据", t, func() {
+			a := &Account{
+				Balance:     decimal.NewFromFloat(100),
+				Status:      1,
+				AccountNo:   ksuid.New().Next().String(),
+				AccountName: "测试资金账户",
+				UserId:      ksuid.New().Next().String(),
+				Username:    sql.NullString{String: "测试用户", Valid: true},
+				AccountType: 2,
+			}
+			id, err := dao.Insert(a)
+			So(err, ShouldBeNil)
+			So(id, ShouldBeGreaterThan, 0)
+			na := dao.GetByUserId(a.UserId, a.AccountType)
+			So(na, ShouldNotBeNil)
+			So(na.Balance.String(), ShouldEqual, a.Balance.String())
+			So(na.CreatedAt, ShouldNotBeNil)
+			So(na.UpdatedAt, ShouldNotBeNil)
+		})
+		return nil
+	})
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
+func TestAccountDao_UpdateBalance(t *testing.T) {
+	err := base.Tx(func(runner *dbx.TxRunner) error {
+		dao := AccountDao{
+			runner: runner,
+		}
+		balance := decimal.NewFromFloat(100)
+		Convey("更新账户余额", t, func() {
+			a := &Account{
+				Balance:     balance,
+				Status:      1,
+				AccountNo:   ksuid.New().Next().String(),
+				AccountName: "测试资金账户",
+				UserId:      ksuid.New().Next().String(),
+				Username:    sql.NullString{String: "测试用户", Valid: true},
+			}
+			id, err := dao.Insert(a)
+			So(err, ShouldBeNil)
+			So(id, ShouldBeGreaterThan, 0)
+
+			// 1. 增加余额
+			Convey("增加余额", func() {
+				amount := decimal.NewFromFloat(10)
+				row,err := dao.UpdateBalance(a.AccountNo, amount)
+				So(err, ShouldBeNil)
+				So(row, ShouldEqual, 1)
+				na := dao.GetOne(a.AccountNo)
+				newBalance := balance.Add(amount)
+				So(na, ShouldNotBeNil)
+				So(na.Balance.String(), ShouldEqual, newBalance.String())
+			})
+			// 2. 扣减余额
+			Convey("扣减余额，余额足够", func() {
+				amount := decimal.NewFromFloat(-10)
+				rows, err := dao.UpdateBalance(a.AccountNo, amount)
+				So(err, ShouldBeNil)
+				So(rows, ShouldEqual, 1)
+				na := dao.GetOne(a.AccountNo)
+				newBalance := balance.Add(amount)
+				So(na, ShouldNotBeNil)
+				So(na.Balance.String(), ShouldEqual, newBalance.String())
+
+			})
+
+			// 3. 扣减余额 余额不足
+			Convey("扣减余额，余额不够", func() {
+				a1 := dao.GetOne(a.AccountNo)
+				So(a1, ShouldNotBeNil)
+				amount := decimal.NewFromFloat(-300)
+				rows, err := dao.UpdateBalance(a.AccountNo, amount)
+				So(err, ShouldBeNil)
+				So(rows, ShouldEqual, 0)
+				a2 := dao.GetOne(a.AccountNo)
+				So(a2, ShouldNotBeNil)
+				So(a1.Balance.String(), ShouldEqual, a2.Balance.String())
+			})
+		})
+		return nil
+	})
+	if err != nil {
+		logrus.Error(err)
+	}
+}
