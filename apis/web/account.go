44266b42cbd28af6e4ce34f2a9a09b614372c1e0
hanxuefeng <hanxuefeng@netjoy.com>
2021-11-09 16:20:44 +0800

    账户相关的api接口

--- apis/web/account.go
+++ apis/web/account.go
@@ -18,6 +18,9 @@ type AccountApi struct {
 func (a *AccountApi) Init() {
 	groupRouter := base.Iris().Party("/v1/account")
 	groupRouter.Post("/create", createHandler)
+	groupRouter.Post("/transfer", transferHandler)
+	groupRouter.Get("/envelope/get", getEnvelopeAccountHandler)
+	groupRouter.Get("/get", getAccountHandler)
 }
 
 func createHandler(ctx iris.Context) {
@@ -42,5 +45,67 @@ func createHandler(ctx iris.Context) {
 		logrus.Error(err)
 	}
 	r.Data = dto
+	ctx.JSON(&r)
+}
+
+func transferHandler(ctx iris.Context) {
+	// 获取请求参数
+	account := services.AccountTransferDTO{}
+	err := ctx.ReadJSON(&account)
+	r := base.Res{
+		Code: base.ResCodeOk,
+	}
+	if err != nil {
+		r.Code = base.ResCodeRequestParamsError
+		r.Message = err.Error()
+		ctx.JSON(&r)
+		logrus.Error(err)
+		return
+	}
+
+	//执行转账逻辑
+	service := services.GetAccountService()
+	status, err := service.Transfer(account)
+	if err != nil {
+		r.Code = base.ResCodeBizError
+		r.Message = err.Error()
+	}
+	r.Data = status
+	ctx.JSON(&r)
+}
+
+//查询红包账户的web接口: /v1/account/envelope/get
+func getEnvelopeAccountHandler(ctx iris.Context) {
+	userId := ctx.URLParam("userId")
+	r := base.Res{
+		Code: base.ResCodeOk,
+	}
+	if userId == "" {
+		r.Code = base.ResCodeRequestParamsError
+		r.Message = "用户ID不能为空"
+		ctx.JSON(&r)
+		return
+	}
+	service := services.GetAccountService()
+	account := service.GetEnvelopeAccountByUserId(userId)
+	r.Data = account
+	ctx.JSON(&r)
+}
+
+//查询账户信息的web接口：/v1/account/get
+func getAccountHandler(ctx iris.Context) {
+	accountNo := ctx.URLParam("accountNo")
+	r := base.Res{
+		Code: base.ResCodeOk,
+	}
+	if accountNo == "" {
+		r.Code = base.ResCodeRequestParamsError
+		r.Message = "账户编号不能为空"
+		ctx.JSON(&r)
+		return
+	}
+	service := services.GetAccountService()
+	account := service.GetAccount(accountNo)
+	r.Data = account
 	ctx.JSON(&r)
 }
--- services/accounts.go
+++ services/accounts.go
@@ -18,6 +18,7 @@ type AccountService interface {
 	Transfer(dto AccountTransferDTO) (TransferedStatus, error)
 	StoreValue(dto AccountTransferDTO) (TransferedStatus, error)
 	GetEnvelopeAccountByUserId(userId string) *AccountDTO
+	GetAccount(accountNo string) *AccountDTO
 }
 
 // TradeParticipator 账户交易的参与者
