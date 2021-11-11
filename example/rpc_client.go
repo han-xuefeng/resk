a17d1f5d79a77f878f405e82f38886d47ee195dc
han-xuefeng <han-xuefeng@hotmail.com>
2021-11-12 00:01:41 +0800

    rpc 收红包的测试用例

--- example/rpc_client.go
+++ example/rpc_client.go
@@ -7,24 +7,43 @@ import (
 	"study-gin/resk/services"
 )
 
-func main()  {
+func main() {
 	c, err := rpc.Dial("tcp", ":8082")
 	if err != nil {
 		logrus.Panic(err)
 	}
+	//sendout(c)
+	receive(c)
+
+}
+
+func receive(c *rpc.Client) {
+	in := services.RedEnvelopeReceiveDTO{
+		EnvelopeNo:   "20mLEDj74jnw9QSzX3AxmQXeDZo",
+		RecvUserId:   "20mLENrRs3cnsS78mgPoaxI4BZ8",
+		RecvUsername: "测试用户10",
+		AccountNo:    "",
+	}
+	out := &services.RedEnvelopeItemDTO{}
+	err := c.Call("EnvelopeRpc.Receive", in, out)
+	if err != nil {
+		logrus.Panic(err)
+	}
+	logrus.Infof("%+v", out)
+}
+func sendout(c *rpc.Client) {
 	in := services.RedEnvelopeSendingDTO{
 		Amount:       decimal.NewFromFloat(1),
-		UserId:       "20iyiDYId85hl3VRurKK3yuQrAV",
+		UserId:       "20mLENrRs3cnsS78mgPoaxI4BZ8",
 		Username:     "测试用户",
 		EnvelopeType: services.GeneralEnvelopeType,
 		Quantity:     2,
 		Blessing:     "",
 	}
 	out := &services.RedEnvelopeActivity{}
-	err = c.Call("EnvelopeRpc.SendOut", in, out)
+	err := c.Call("EnvelopeRpc.SendOut", in, &out)
 	if err != nil {
 		logrus.Panic(err)
 	}
-	c.Close()
-	logrus.Info(out)
+	logrus.Infof("%+v", out)
 }
